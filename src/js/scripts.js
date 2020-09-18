cffbcf5e0224c91369cfea90c236c5c605c8cf21
Utkarsh Singh <utkarsh.singh0609@gmail.com>
2020-09-17 23:13:38 -0500

    Final changes before publishing

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -42,16 +42,16 @@
   // Magnific popup calls
   $('#portfolio').magnificPopup({
     delegate: 'a',
-    type: 'image',
+    type: 'file',
     tLoading: 'Loading image #%curr%...',
-    mainClass: 'mfp-img-mobile',
+    mainClass: 'mfp-file-mobile',
     gallery: {
       enabled: true,
       navigateByImgClick: true,
       preload: [0, 1]
     },
     image: {
-      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+      tError: '<a href="%url%">Download #%curr%</a> here.'
     }
   });
 
